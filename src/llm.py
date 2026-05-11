da513610bee42ae5862e66243ad82cc069c2fb18
Afreen Shaik <afreenshaik0921@gmail.com>
2026-05-11 12:41:18 +0530

    your message

--- src/llm.py
+++ src/llm.py
@@ -1,16 +1,24 @@
 import os
+from dotenv import load_dotenv
 from huggingface_hub import InferenceClient
 
+load_dotenv()
+
 client = InferenceClient(
-    token=os.getenv("HF_TOKEN")
+    api_key=os.getenv("HF_TOKEN")
 )
 
 def generate_answer(prompt):
 
-    response = client.text_generation(
-        prompt,
-        model="google/flan-t5-base",
-        max_new_tokens=80
+    response = client.chat_completion(
+        model="HuggingFaceH4/zephyr-7b-beta",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        max_tokens=80
     )
 
-    return response
+    return response.choices[0].message.content
