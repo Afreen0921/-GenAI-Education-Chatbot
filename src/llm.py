43ad37caab07253a6a1bb7ef66f679ef73ab6567
Afreen Shaik <afreenshaik0921@gmail.com>
2026-05-11 13:24:36 +0530

    fixed flan pipeline properly

--- src/llm.py
+++ src/llm.py
@@ -1,7 +1,7 @@
 from transformers import pipeline
 
 generator = pipeline(
-    task="text-generation",
+    "text2text-generation",
     model="google/flan-t5-base"
 )
 
@@ -9,7 +9,8 @@ def generate_answer(prompt):
 
     result = generator(
         prompt,
-        max_new_tokens=50
+        max_new_tokens=60,
+        do_sample=False
     )
 
-    return result[0]["generated_text"]
+    return result[0]["generated_text"].strip()
